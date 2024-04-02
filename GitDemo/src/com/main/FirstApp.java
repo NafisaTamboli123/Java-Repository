21fda3ad34867dc4c02d0396866a3f09502edd62
NafisaTamboli123 <165625586+NafisaTamboli123@users.noreply.github.com>
2024-04-02 09:37:44 +0530

    Update FirstApp.java
    By Nafisa Tamboli

--- GitDemo/src/com/main/FirstApp.java
+++ GitDemo/src/com/main/FirstApp.java
@@ -2,12 +2,13 @@ package com.main;
 
 public class FirstApp {
 
-	public static void main(String[] args) 
-	{
-		public void m1()
+	public void m1()
 			{
 			System.out.println("This is M1 method");
 			}
+	public static void main(String[] args) 
+	{
+		
 		System.out.println("This is Git First App");
 	}
 
