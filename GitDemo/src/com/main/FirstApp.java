bef5607f5eba640e0aa1507711e99b0dd97425ee
NafisaTamboli123 <165625586+NafisaTamboli123@users.noreply.github.com>
2024-04-02 09:28:51 +0530

    Update FirstApp.java
    M1 method added

--- GitDemo/src/com/main/FirstApp.java
+++ GitDemo/src/com/main/FirstApp.java
@@ -4,6 +4,10 @@ public class FirstApp {
 
 	public static void main(String[] args) 
 	{
+		public void m1()
+			{
+			System.out.println("This is M1 method");
+			}
 		System.out.println("This is Git First App");
 	}
 
